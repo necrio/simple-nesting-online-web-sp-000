56329c4d85dbffe26ae2b5e1cadc2d2ff404328e
Keith Isham <kisham8899@gmail.com>
2019-05-03 20:11:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 return programmer_hash[:alan_kay][:known_for] = "GUI"
-		 return programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+		 
 
 
 end
@@ -130,5 +130,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 end
